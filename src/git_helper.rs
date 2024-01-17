cfdd7b150eaa8e8cda20a18f3c066d857bc8347f
Ashis <pradhanashish95@gmail.com>
2024-01-17 23:41:46 +0530

    test

--- src/git_helper.rs
+++ src/git_helper.rs
@@ -1,4 +1,5 @@
-use std::process::{Command, Stdio};
+use std::process::{Command};
+use colored::*;
 
 pub fn git_add() {
     let add_status = Command::new("git")
@@ -6,26 +7,26 @@ pub fn git_add() {
         .output()
         .expect("Something went wrong while adding");
     if !add_status.status.success() {
-        panic!("{:?}", String::from_utf8(add_status.stderr).unwrap());
+        panic!("{:?}", String::from_utf8(add_status.stderr).unwrap().red());
     }
 }
 
-pub fn git_commit(message: String) {
+pub fn git_commit(message: &str) {
     let commit_status = Command::new("git")
         .args(["commit", "-m", &message])
         .output()
         .expect("Something went wrong while commiting");
     if !commit_status.status.success() {
-        panic!("{:?}", String::from_utf8(commit_status.stderr).unwrap());
+        panic!("{:?}", String::from_utf8(commit_status.stderr).unwrap().red());
     }
 }
 
-pub fn git_push(branch: String) {
+pub fn git_push(branch: &str) {
     let push_status = Command::new("git")
         .args(["push", "origin", &branch])
         .output()
         .expect("Something went wrong while pushing");
     if !push_status.status.success() {
-        panic!("{:?}", String::from_utf8(push_status.stderr).unwrap());
+        panic!("{:?}", String::from_utf8(push_status.stderr).unwrap().red());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ fn run_git() {
     let branch = util_helper::get_branch_from_user();
     let commit_message = util_helper::get_commit_message_from_user();
     git_helper::git_add();
-    git_helper::git_commit(commit_message);
-    git_helper::git_push(branch);
+    git_helper::git_commit(&commit_message);
+    git_helper::git_push(&branch);
 }
 fn main() {
     println!("Welcome to GIT CLI!!");
